566b18ed945b6002101297bd86f9814b83478b73
pathum <awpathum@gmail.com>
2020-08-05 20:49:47 +0530

    Adapter done

---
+++ DesignPatterns/src/Adapter/School.java
@@ -0,0 +1,10 @@
+package Adapter;
+
+public class School {
+    public static void main(String[] args) {
+        Pen p  = new PenAdapter();
+        AssignmentWork aw = new AssignmentWork();
+        aw.setP(p);
+        aw.writeAssignment("My Assignment");
+    }
+}
